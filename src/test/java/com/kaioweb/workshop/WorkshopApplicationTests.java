dd830ee00b497f5ec604811ebf1134b2e3041a92
Kaio Cavalcanti <kayollindoso@gmail.com>
2024-01-20 01:30:36 -0300

    Project created

---
+++ src/test/java/com/kaioweb/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaioweb.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
