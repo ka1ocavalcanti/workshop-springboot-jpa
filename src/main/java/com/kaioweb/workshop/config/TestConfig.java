cbb05bea2ae1446398895aea3a2e0e7262dbf672
Kaio Cavalcanti <kayollindoso@gmail.com>
2024-01-29 22:14:26 -0300

    Payment, one-to-one association

--- src/main/java/com/kaioweb/workshop/config/TestConfig.java
+++ src/main/java/com/kaioweb/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kaioweb.workshop.entities.Category;
 import com.kaioweb.workshop.entities.Order;
 import com.kaioweb.workshop.entities.OrderItem;
+import com.kaioweb.workshop.entities.Payment;
 import com.kaioweb.workshop.entities.Product;
 import com.kaioweb.workshop.entities.User;
 import com.kaioweb.workshop.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-22T15:21:22Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
